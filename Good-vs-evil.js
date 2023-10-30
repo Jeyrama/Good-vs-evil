cc55902f7894f795292698ae814218a007d119c2
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-29 23:54:48 -0700

    Update Good-vs-evil.js

--- Good-vs-evil.js
+++ Good-vs-evil.js
@@ -41,3 +41,17 @@ Return "Battle Result: Good triumphs over Evil" if good wins, "Battle Result: Ev
 
 
 // Solution
+
+function goodVsEvil(good, evil) {  
+  let getWorth = function( side, worth ) {
+    return side.split(' ').reduce( function(result, value, index) { 
+      return result + (worth[index] * value);
+    }, 0);
+  }
+
+  let result = getWorth( good, [1,2,3,3,4,10] ) - getWorth( evil, [1,2,2,2,3,5,10] );
+
+  return result > 0 ? "Battle Result: Good triumphs over Evil" :
+         result < 0 ? "Battle Result: Evil eradicates all trace of Good" :
+                      "Battle Result: No victor on this battle field";
+}
